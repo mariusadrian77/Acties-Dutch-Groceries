34a920c98650c908496f8a4ac931d9573c4c3521
mariusadrian77 <m.a.lupulescu@gmail.com>
2025-03-07 00:09:22 +0000

    - Ah bonus and Ah normal products scrapped.

--- supconnector.py
+++ supconnector.py
@@ -2,4 +2,4 @@ from supermarktconnector.ah import AHConnector
 
 connector = AHConnector()
 # connector.get_categories()
-print(connector.get_categories())
+print(connector.get_product_details(54074))
